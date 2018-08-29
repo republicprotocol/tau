b7f8fe7c08dfac60728687cb1d4e506c1be436c5
Ross Pure <ross.pure@gmail.com>
2018-08-29 15:32:25 +1000

    core/vss/algebra: combined two polynomial creation functions

--- core/vss/algebra/polynomial.go
+++ core/vss/algebra/polynomial.go
@@ -11,10 +11,10 @@ type Polynomial struct {
 	coefficients []*big.Int
 }
 
-// NewPoly creates a new polynomial over the given field with the given
+// NewPolynomial creates a new polynomial over the given field with the given
 // coefficients. If any of the coefficients are not in the field, then the
 // function will panic.
-func NewPoly(field *Fp, coefficients []*big.Int) Polynomial {
+func NewPolynomial(field *Fp, coefficients []*big.Int) Polynomial {
 	for _, c := range coefficients {
 		if !field.InField(c) {
 			panic("coefficient must be a field element")
@@ -23,52 +23,21 @@ func NewPoly(field *Fp, coefficients []*big.Int) Polynomial {
 	return Polynomial{field, coefficients}
 }
 
-// NewRandomPoly creates a new polynomial over the given field with random
-// coefficients. The coefficient for the x^degree term is gauranteed to be
-// non-zero, so that the polynomial is always of the given degree.
-func NewRandomPoly(field *Fp, degree int) Polynomial {
+// NewRandomPolynomial creates a new polynomial over the given field with random
+// coefficients. A secret can be optionally provided, and will set the constant
+// term to be that secret. If more than one argument for secret is given, the
+// function will panic. The coefficient for the x^degree term is gauranteed to
+// be non-zero, so that the polynomial is always of the given degree.
+func NewRandomPolynomial(field *Fp, degree int, secret ...*big.Int) Polynomial {
+	if len(secret) > 1 {
+		panic("maximum of one secret")
+	}
 	var err error
+
 	coefficients := make([]*big.Int, degree+1)
-	for i := 0; i <= degree; i++ {
-		coefficients[i], err = rand.Int(rand.Reader, field.prime)
-		if err != nil {
-			// This should never occur because it is impossible to construct a
-			// field with a prime that is not positive
-			panic(err)
-		}
+	if len(secret) != 0 {
+		coefficients[0] = secret[0]
 	}
-
-	// Make sure that the leading coefficient is not zero, otherwise the
-	// polynomial would have a smaller degree
-	for coefficients[degree].Sign() == 0 {
-		coefficients[degree], err = rand.Int(rand.Reader, field.prime)
-		if err != nil {
-			// This should never occur because it is impossible to construct a
-			// field with a prime that is not positive
-			panic(err)
-		}
-	}
-
-	return Polynomial{field, coefficients}
-}
-
-// NewRandomWithSecret creates a new polynomial over the given field where the
-// constant term is the given secret. If the secret is not an element of the
-// field, the function will panic. The remaining coefficients are random field
-// elements. The polynomial is gauranteed to have the given degree by ensuring
-// that the x^degree term is not zero.
-func NewRandomWithSecret(field *Fp, degree int, secret *big.Int) Polynomial {
-	if !field.InField(secret) {
-		panic("secret must be a field element")
-	}
-
-	var err error
-	coefficients := make([]*big.Int, degree+1)
-
-	// The secret is the constant term
-	coefficients[0] = secret
-
-	// The rest of the coefficients are random
 	for i := 1; i <= degree; i++ {
 		coefficients[i], err = rand.Int(rand.Reader, field.prime)
 		if err != nil {
