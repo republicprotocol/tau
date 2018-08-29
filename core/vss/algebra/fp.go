0cae3be7b976ec06de16493a8661a763cb6ea144
Ross Pure <ross.pure@gmail.com>
2018-08-29 15:31:45 +1000

    core/vss/algebra: function for creating random field element

--- core/vss/algebra/fp.go
+++ core/vss/algebra/fp.go
@@ -1,6 +1,7 @@
 package algebra
 
 import (
+	"crypto/rand"
 	"math/big"
 )
 
@@ -12,12 +13,14 @@ type Fp struct {
 }
 
 // NewField returns a new field object. The field will be the integers modulo
-// the given prime. If the given prime is not a positive number, the function
-// will panic.
+// the given prime. If the given prime is probably not a prime, as determined by
+// big.ProbablyPrime, then the function panics. If the prime is in fact a prime,
+// then big.ProbablyPrime will always return true, and so for correctly inputs
+// the function will never panic.
 func NewField(prime *big.Int) Fp {
 	// The prime must be a positive number
-	if prime.Sign() != 1 {
-		panic("prime must be a positive integer")
+	if !prime.ProbablyPrime(32) {
+		panic("given prime is probably not prime")
 	}
 	return Fp{prime}
 }
@@ -25,10 +28,19 @@ func NewField(prime *big.Int) Fp {
 // InField checks whether a given integer is in the field. This will be the case
 // when the integer is positive and less than the prime defining the field.
 func (f *Fp) InField(x *big.Int) bool {
-	if x.Cmp(f.prime) != -1 {
+	if x.Cmp(f.prime) != -1 || x.Sign() == -1 {
 		return false
 	}
 	return true
+}
+
+// Random returns a random element in the given field.
+func (f *Fp) Random() *big.Int {
+	// This should never return an error because it is impossible to construct a
+	// field with a prime that is not positive
+	r, _ := rand.Int(rand.Reader, f.prime)
+
+	return r
 }
 
 // Add sets c = a + b
@@ -63,11 +75,10 @@ func (f *Fp) MulInv(a, c *big.Int) {
 	if !f.InField(a) {
 		panic("cannot find the inverse of an element that is not in the field")
 	}
-	check := c.ModInverse(a, f.prime)
-	if check == nil {
-		// This will only occurr when f.prime is in fact not a prime
-		panic("field element not relatively prime to field prime")
-	}
+
+	// This should never fail because it is not possible to construct a field
+	// with a non-prime (with high probability)
+	c.ModInverse(a, f.prime)
 }
 
 // Sub sets c = a - b
@@ -75,8 +86,8 @@ func (f *Fp) Sub(a, b, c *big.Int) {
 	if !f.InField(a) || !f.InField(b) {
 		panic("cannot subtract elements that are not in the field")
 	}
-	c.Sub(a, b)
-	c.Mod(c, f.prime)
+	f.Neg(b, c)
+	f.Add(a, c, c)
 }
 
 // Div sets c = a/b = a*(b^-1)
@@ -84,7 +95,6 @@ func (f *Fp) Div(a, b, c *big.Int) {
 	if !f.InField(a) || !f.InField(b) {
 		panic("cannot subtract elements that are not in the field")
 	}
-	binv := big.NewInt(0)
-	f.MulInv(b, binv)
-	f.Mul(a, binv, c)
+	f.MulInv(b, c)
+	f.Mul(a, c, c)
 }
