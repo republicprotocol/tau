a84fa2ebc14833168550f2fbbcec450f28f1ff0a
Ross Pure <ross.pure@gmail.com>
2018-09-06 10:10:38 +1000

    core/vss: documentation

--- core/vss/vss.go
+++ core/vss/vss.go
@@ -8,19 +8,26 @@ import (
 	"github.com/republicprotocol/smpc-go/core/vss/shamir"
 )
 
+// A VShare is a Shamir share that can be verified to be correctly shared. The
+// verification is based on Pedersen commitments.
 type VShare struct {
 	commitments []algebra.FpElement
 	share, t    shamir.Share
 }
 
+// Share is a getter for the share field of a VShare struct.
 func (vs *VShare) Share() shamir.Share {
 	return vs.share
 }
 
+// SetShare is a setter for the share field of a VShare struct. The main purpose
+// of this method is for testing malicious behaviour.
 func (vs *VShare) SetShare(share shamir.Share) {
 	vs.share = share
 }
 
+// SetCommitments is a setter for the commitment field of a VShare struct. The
+// main purpose of this method is for testing malicious behaviour.
 func (vs *VShare) SetCommitments(commitments []algebra.FpElement) {
 	vs.commitments = commitments
 }
@@ -28,6 +35,8 @@ func (vs *VShare) SetCommitments(commitments []algebra.FpElement) {
 // VShares is a list of VShare structs.
 type VShares []VShare
 
+// Share creates a list of verifiable shares given a Pedersen scheme, secret,
+// and the n and k that define the threshold sharing scheme.
 func Share(ped *pedersen.Pedersen, secret algebra.FpElement, n, k uint64) VShares {
 	field := secret.Field()
 	polyF := algebra.NewRandomPolynomial(field, uint(k-1), secret)
@@ -51,6 +60,9 @@ func Share(ped *pedersen.Pedersen, secret algebra.FpElement, n, k uint64) VShare
 	return shares
 }
 
+// Verify returns true if a verifiable share is correct in the Pedersen scheme,
+// and false otherwise. If the list of commitments is empty (this cannot happen
+// in the honest use case) then it will panic indirectly through evaluate().
 func Verify(ped *pedersen.Pedersen, vshare VShare) bool {
 	expected := ped.Commit(vshare.share.Value(), vshare.t.Value())
 	actual := evaluate(ped, vshare.commitments, vshare.share)
@@ -58,6 +70,8 @@ func Verify(ped *pedersen.Pedersen, vshare VShare) bool {
 	return expected.Eq(actual)
 }
 
+// The evaluate is a convenience function that computes the evaluation of the
+// polynomial in the exponents of g and h from the Pedersen scheme.
 func evaluate(ped *pedersen.Pedersen, commitments []algebra.FpElement, share shamir.Share) algebra.FpElement {
 	if len(commitments) == 0 {
 		panic("cannot verify against an empty list of commitments")
