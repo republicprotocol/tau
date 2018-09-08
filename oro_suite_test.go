7e73bfc59eabf2f251254ab608a8ee5d1ed09cf6
Loong <loong@republicprotocol.com>
2018-09-08 18:53:49 +1000

    oro: renamed top level files

--- oro_suite_test.go
+++ oro_suite_test.go
@@ -1,4 +1,4 @@
-package smpc_test
+package oro_test
 
 import (
 	"testing"
@@ -9,5 +9,5 @@ import (
 
 func TestSmpcGo(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "SmpcGo Suite")
+	RunSpecs(t, "Oro Suite")
 }
