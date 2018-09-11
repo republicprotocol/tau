8ff23b248a6e8b7f80ba8edfcdbdb5b7974fa044
Ross Pure <ross.pure@gmail.com>
2018-09-11 10:15:35 +1000

    core/vm/process: macros for and, or, not

--- core/vm/process/macro.go
+++ core/vm/process/macro.go
@@ -1,12 +1,35 @@
 package process
 
-// var Not = Code{
-// 	InstPush(),
-// }
+import (
+	"math/big"
 
-var Or = Code{}
+	"github.com/republicprotocol/oro-go/core/vss/algebra"
+)
 
-// var And = Code{
-// 	InstNeg(),
-// 	InstPush(NewValuePublic())
-// }
+func Not(field algebra.Fp) Inst {
+	code := Code{
+		InstNeg(),
+		InstPush(NewValuePublic(field.NewInField(big.NewInt(1)))),
+		InstAdd(),
+	}
+	return InstMacro(code)
+}
+
+func Or() Inst {
+	code := Code{
+		InstCopy(2),
+		InstGenerateRn(),
+		InstMul(),
+		InstSub(),
+		InstAdd(),
+	}
+	return InstMacro(code)
+}
+
+func And() Inst {
+	code := Code{
+		InstGenerateRn(),
+		InstMul(),
+	}
+	return InstMacro(code)
+}
