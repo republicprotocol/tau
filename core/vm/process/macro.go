53242b616d4967c84c8758329779f51e5e4552e4
Ross Pure <ross.pure@gmail.com>
2018-09-13 12:19:55 +1000

    core/vm/process: extract carry out circuit from cmp macro

--- core/vm/process/macro.go
+++ core/vm/process/macro.go
@@ -1,6 +1,8 @@
 package process
 
 import (
+	"fmt"
+	"log"
 	"math/big"
 
 	"github.com/republicprotocol/oro-go/core/vss/algebra"
@@ -17,11 +19,11 @@ func MacroNot(field algebra.Fp) Inst {
 
 func MacroOr() Inst {
 	code := Code{
-		InstCopy(2),
-		InstGenerateRn(),
-		InstMul(),
-		InstSub(),
-		InstAdd(),
+		InstCopy(2),      // ...a, b, a, b]
+		InstGenerateRn(), // ...a, b, a, b, r]
+		InstMul(),        // ...a, b, ab]
+		InstSub(),        // ...a, b - ab]
+		InstAdd(),        // ...a + b - ab]
 	}
 	return InstMacro(code)
 }
@@ -111,13 +113,12 @@ func MacroOpCLA() Inst {
 	return InstMacro(code)
 }
 
-func MacroCmp64(field algebra.Fp, offset Addr) Inst {
-	code := make(Code, 0, 320+4+7*64+2)
-	for i := 0; i < 64; i++ {
+func MacroBitwiseCOut(field algebra.Fp, offset Addr, bits uint) Inst {
+	code := make(Code, 0)
+	for i := uint(0); i < bits; i++ {
 		c := Code{
 			InstLoad(offset + Addr(2*i)),    // ...ai]
 			InstLoad(offset + Addr(2*i+1)),  // ...ai, bi]
-			MacroNot(field),                 // ...ai, !bi]
 			MacroPropGen(),                  // ...pi, gi]
 			InstStore(offset + Addr(2*i+1)), // ...pi]
 			InstStore(offset + Addr(2*i)),   // ...]
@@ -125,16 +126,8 @@ func MacroCmp64(field algebra.Fp, offset Addr) Inst {
 		code = append(code, c...)
 	}
 
-	c := Code{
-		InstLoad(offset),            // ...p0]
-		InstLoad(offset + Addr(1)),  // ...p0, g0]
-		MacroOr(),                   // ...p0 | g0]
-		InstStore(offset + Addr(1)), // ...]
-	}
-	code = append(code, c...)
-
-	for i := 32; i > 0; i /= 2 {
-		for j := 0; j < i; j++ {
+	for i := bits / 2; i > 0; i /= 2 {
+		for j := uint(0); j < i; j++ {
 			c := Code{
 				InstLoad(offset + Addr(4*j)),    // ...pj]
 				InstLoad(offset + Addr(4*j+1)),  // ...pj, gj]
@@ -148,11 +141,69 @@ func MacroCmp64(field algebra.Fp, offset Addr) Inst {
 		}
 	}
 
+	code = append(code, InstLoad(offset+Addr(1))) // ...G]
+
+	return InstMacro(code)
+}
+
+func MacroCmp64(field algebra.Fp, offset Addr) Inst {
+	str := "[debug] memory operations:\n"
+	code := make(Code, 0, 320+4+7*64+2)
+	for i := 0; i < 64; i++ {
+		c := Code{
+			InstLoad(offset + Addr(2*i)),    // ...ai]
+			InstLoad(offset + Addr(2*i+1)),  // ...ai, bi]
+			MacroNot(field),                 // ...ai, !bi]
+			MacroPropGen(),                  // ...pi, gi]
+			InstStore(offset + Addr(2*i+1)), // ...pi]
+			InstStore(offset + Addr(2*i)),   // ...]
+		}
+		code = append(code, c...)
+		str += fmt.Sprintf("loading a%v from %v\n", i, offset+Addr(2*i))
+		str += fmt.Sprintf("loading b%v from %v\n", i, offset+Addr(2*i+1))
+		str += fmt.Sprintf("storing g%v at %v\n", i, offset+Addr(2*i+1))
+		str += fmt.Sprintf("storing p%v at %v\n", i, offset+Addr(2*i))
+	}
+
+	c := Code{
+		InstLoad(offset),            // ...p0]
+		InstLoad(offset + Addr(1)),  // ...p0, g0]
+		MacroOr(),                   // ...p0 | g0]
+		InstStore(offset + Addr(1)), // ...]
+	}
+	code = append(code, c...)
+	str += fmt.Sprintf("loading p0 from %v\n", offset)
+	str += fmt.Sprintf("loading g0 from %v\n", offset+Addr(1))
+	str += fmt.Sprintf("storing g0 at %v\n", offset+Addr(1))
+
+	for i := 32; i > 0; i /= 2 {
+		for j := 0; j < i; j++ {
+			c := Code{
+				InstLoad(offset + Addr(4*j)),    // ...pj]
+				InstLoad(offset + Addr(4*j+1)),  // ...pj, gj]
+				InstLoad(offset + Addr(4*j+2)),  // ...pj, gj, p{j+1}]
+				InstLoad(offset + Addr(4*j+3)),  // ...pj, gj, p{j+1}, g{j+1}]
+				MacroOpCLA(),                    // ...Pj, Gj]
+				InstStore(offset + Addr(2*j+1)), // ...Pj]
+				InstStore(offset + Addr(2*j)),   // ...]
+			}
+			code = append(code, c...)
+			str += fmt.Sprintf("loading p%v from %v\n", j, offset+Addr(4*j))
+			str += fmt.Sprintf("loading g%v from %v\n", j, offset+Addr(4*j+1))
+			str += fmt.Sprintf("loading p%v from %v\n", j+1, offset+Addr(4*j+2))
+			str += fmt.Sprintf("loading g%v from %v\n", j+1, offset+Addr(4*j+3))
+			str += fmt.Sprintf("storing G%v at %v\n", j, offset+Addr(2*j+1))
+			str += fmt.Sprintf("storing P%v at %v\n", j, offset+Addr(2*j))
+		}
+	}
+
 	c = Code{
 		InstLoad(offset + Addr(1)), // ...G]
 		MacroNot(field),            // ..!G]
 	}
 	code = append(code, c...)
+	str += fmt.Sprintf("loading G from %v\n", offset+Addr(1))
+	log.Println(str)
 
 	return InstMacro(code)
 }
