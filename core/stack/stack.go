73f1848f3ac0aee5710351d353ab71c9d4441db8
Ross Pure <ross.pure@gmail.com>
2018-09-13 12:18:24 +1000

    core/stack: instruction to peek into the stack

--- core/stack/stack.go
+++ core/stack/stack.go
@@ -65,6 +65,16 @@ func (stack *Stack) Pop() (Element, error) {
 	return elem, nil
 }
 
+func (stack *Stack) Peek(offset int) (Element, error) {
+	if offset < 0 {
+		return nil, ErrStackOverflow
+	}
+	if offset >= stack.free {
+		return nil, ErrStackUnderflow
+	}
+	return stack.elems[stack.free-offset-1], nil
+}
+
 // IsFull returns true when the Stack is full, otherwise it returns false.
 // Pushing to a full Stack will result in a stack overflow.
 func (stack *Stack) IsFull() bool {
