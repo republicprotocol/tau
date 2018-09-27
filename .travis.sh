4a10fe19398343faea189b15cc9445a0ee0095a2
Loong <loong@republicprotocol.com>
2018-09-27 16:12:42 +1000

    travis: disable vet and lint

--- .travis.sh
+++ .travis.sh
@@ -3,8 +3,8 @@
 # This is helpful because Travis CI often finds issues that our own local tests
 # do not.
 
-go vet ./...
-golint -set_exit_status `go list ./... | grep -Ev "(vendor)"`
+# go vet ./...
+# golint -set_exit_status `go list ./... | grep -Ev "(vendor)"`
 
 go build ./...
 GOMAXPROCS=1 CI=true ginkgo --cover core/collection/buffer \
