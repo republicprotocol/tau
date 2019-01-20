b56b83c4e2d1937f78a9829294801cf8d3f302f0
Divya Mary <divyakoshy05@gmail.com>
2019-01-20 11:59:34 +1100

    core/task: Message interface exposes MessageID

--- core/task/message.go
+++ core/task/message.go
@@ -27,6 +27,9 @@ type Message interface {
 	// IsMessage is a marker function. It does nothing, but is used to prevent
 	// erroneously sending non-Message types between Tasks.
 	IsMessage()
+
+	// MessageID returns the `MessageID` of the Message.
+	MessageID() MessageID
 }
 
 // A MessageBatch is a Message containing multiple Messages. During reduction, a
@@ -43,21 +46,32 @@ func NewMessageBatch(messages []Message) Message {
 func (message MessageBatch) IsMessage() {
 }
 
+// MessageID implements the Message interface for MessageBatch.
+func (message MessageBatch) MessageID() MessageID {
+	return [40]byte{}
+}
+
 // An Error is a Message wrapper type for sending errors between Tasks. It
 // automatically catches the stack trace to help with debugging the origin of
 // the error.
 type Error struct {
-	error
+	err error
+	id  MessageID
 }
 
 // NewError returns an Error. The stack trace is captured at the moment this
 // function is called.
-func NewError(err error) Message {
-	return Error{fmt.Errorf("err = %v; stack = %v", err, string(debug.Stack()))}
+func NewError(err error, messageID MessageID) Message {
+	return Error{fmt.Errorf("err = %v; stack = %v", err, string(debug.Stack())), messageID}
 }
 
 // IsMessage implements the Message interface for Error.
 func (message Error) IsMessage() {
+}
+
+// MessageID implements the Message interface for Error.
+func (message Error) MessageID() MessageID {
+	return message.id
 }
 
 // RandomMessage is an empty message that can be used for testing internally and
@@ -66,20 +80,31 @@ type RandomMessage struct {
 }
 
 // IsMessage implements the Message interface for Random Message.
-func (msg RandomMessage) IsMessage() {
+func (message RandomMessage) IsMessage() {
+}
+
+// MessageID implements the Message interface for Random Message.
+func (message RandomMessage) MessageID() MessageID {
+	return [40]byte{}
 }
 
 // A Tick is a Message that is used to signal the passing of time. Tasks should
 // rely on Ticks to keep track of time, instead of tracking it internally.
 type Tick struct {
-	time.Time
+	time      time.Time
+	messageID MessageID
 }
 
 // NewTick returns a Tick for a moment in time.
-func NewTick(time time.Time) Message {
-	return Tick{time}
+func NewTick(time time.Time, messageID MessageID) Message {
+	return Tick{time, messageID}
 }
 
 // IsMessage implements the Message interface for Tick.
 func (message Tick) IsMessage() {
+}
+
+// MessageID implements the Message interface for Tick.
+func (message Tick) MessageID() MessageID {
+	return message.messageID
 }
